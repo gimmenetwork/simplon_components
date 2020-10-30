c27be8b66f14904bdc83c8fffe12891d705fc738
Kostiantyn Stupak <konstantin.stupak@gimmemore.com>
2020-10-30 12:41:40 +0100

    bumping composer dependency

--- src/ComponentInstallerPlugin.php
+++ src/ComponentInstallerPlugin.php
@@ -32,4 +32,8 @@ class ComponentInstallerPlugin implements PluginInterface
         $installer = new Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
+
+    public function deactivate(Composer $composer, IOInterface $io) {}
+
+    public function uninstall(Composer $composer, IOInterface $io) {}
 }
